7296df57fa233ba7e73a4d64a763830a63aecd36
MIGUEL ANGEL VASQUEZ JIMENEZ <miguel_seus@hotmail.com>
2017-11-04 10:18:07 -0500

    Se practico  manejo del DOM con javascript puro

--- Scripts/Contoso.js
+++ Scripts/Contoso.js
@@ -1,1 +1,14 @@
-alert('hola');
+document.getElementById("btnAddElementList").addEventListener('click', function(){
+    var list = document.getElementById('elementList');
+    var oneLi = document.createElement('li');
+    var twoLi = document.createElement('li');
+    oneLi.innerHTML = "Element 1";
+    twoLi.innerHTML = "Element 2";
+    list.appendChild(oneLi);
+    list.insertBefore(twoLi, list.firstChild);
+});
+
+document.getElementById("btnRemoveFirstElement").onclick = function(){
+    var list = document.getElementById("elementList");
+    list.removeChild(list.firstChild);
+}
